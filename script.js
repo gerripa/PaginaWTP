8de371a726907d8535d9dbcb8e381e5a31d27d78
Guadalupe <grripa@itba.edu.ar>
2025-10-06 17:17:50 -0300

    ccc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Amélie Atelier — inicio cargado correctamente");
 
+// VIDEO: control de sonido
 const video = document.getElementById('heroVideo');
 const soundBtn = document.getElementById('soundToggle');
 const soundIcon = document.getElementById('soundIcon');
@@ -9,13 +10,22 @@ soundBtn.addEventListener('click', () => {
   soundIcon.textContent = video.muted ? '🔇' : '🔊';
 });
 
-// Menú hamburguesa
+// MENÚ HAMBURGUESA
 const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.getElementById('navLinks');
+const overlay = document.getElementById('overlay');
 
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+  overlay.classList.toggle('active');
 });
+
+// Cerrar menú si se toca el overlay
+overlay.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
 
 
 
