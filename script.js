57c5c6720f3ff6444a610eb7033dbd99aae7ae1b
Guadalupe <grripa@itba.edu.ar>
2025-10-06 01:13:15 -0300

    cambios

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const header = document.getElementById('header');
-const videoSection = document.getElementById('video');
 
-window.addEventListener('scroll', () => {
-  const videoTop = videoSection.offsetTop;
-  const scrollPos = window.scrollY;
-
-  if (scrollPos >= videoTop - 100) {
-    header.classList.add('show-logo');
-  } else {
-    header.classList.remove('show-logo');
-  }
-});
 
 
 
