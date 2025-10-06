0d9400d7e26c192d8cf350325e2277ec24dcef2d
Guadalupe <grripa@itba.edu.ar>
2025-10-06 12:58:12 -0300

    cambios 10/6

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+console.log("Amélie Atelier — inicio cargado correctamente");
+
+const video = document.getElementById('heroVideo');
+const soundBtn = document.getElementById('soundToggle');
+const soundIcon = document.getElementById('soundIcon');
+
+soundBtn.addEventListener('click', () => {
+  video.muted = !video.muted;
+  soundIcon.textContent = video.muted ? '🔇' : '🔊';
+});
+
 
 
 
